562e131fc8957740a0deeea90acd42b17da427ed
kohearn-vail <54598571+kohearn-vail@users.noreply.github.com>
2021-05-10 15:07:03 -0500

    'FreeClimbAccountToken' -> 'FreeClimbApiKey'

--- Program.cs
+++ Program.cs
@@ -11,11 +11,11 @@ namespace StreamRecording
       try
       {
         string FreeClimbAccountId = System.Environment.GetEnvironmentVariable("ACCOUNT_ID");
-        string FreeClimbAccountToken = System.Environment.GetEnvironmentVariable("API_KEY");
+        string FreeClimbApiKey = System.Environment.GetEnvironmentVariable("API_KEY");
         string RecordingId = "";
 
         // Create FreeClimbClient object
-        FreeClimbClient client = new FreeClimbClient(FreeClimbAccountId, FreeClimbAccountToken);
+        FreeClimbClient client = new FreeClimbClient(FreeClimbAccountId, FreeClimbApiKey);
 
         // Invoke get method with AudioReturn format of Stream to obtain IO stream of audio
         Stream stream = client.getRecordingsRequester.stream(RecordingId);
